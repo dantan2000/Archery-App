793923171589bb2a978b89deb75f3a058b306471
Daniel Tan <tan.da@husky.neu.edu>
2020-02-09 18:30:07 -0800

    Initial Commit

---
+++ Archery App/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Archery App
+//
+//  Created by daniel tan on 2/9/20.
+//  Copyright © 2020 Daniel Tan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Archery AppTests/Archery_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Archery_AppTests.swift
+//  Archery AppTests
+//
+//  Created by daniel tan on 2/9/20.
+//  Copyright © 2020 Daniel Tan. All rights reserved.
+//
+
+import XCTest
+@testable import Archery_App
+
+class Archery_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
